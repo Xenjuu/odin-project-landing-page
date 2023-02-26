86cd030420dae619c577e417c655d88c1cfe4152
JM <johnmarkh636@gmail.com>
2023-02-27 00:03:01 +0800

    Creatint sidenav in mobile screen size

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidebar").style.width = "80vw";
+  document.getElementById('btn-open').style.display = 'none';
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById('btn-open').style.display = 'block';
+}
